c29c099bdfbc8f464bb0c80e917f6eb563c493f6
operator1 <operator1@users.noreply.github.com>
2015-04-04 12:17:29 +0100

    Create Extended.java

---
+++ op1/src/main/java/com/op1/iff/types/Extended.java
@@ -0,0 +1,11 @@
+package com.op1.iff.types;
+
+/**
+ * 80 bit IEEE Standard 754 floating point number (Standard Apple Numeric Environment [SANE] data type Extended).
+ */
+public class Extended extends DataType {
+
+    public Extended(byte[] bytes) {
+        super(bytes);
+    }
+}
