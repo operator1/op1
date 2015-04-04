cde1612884877019fa3e3856e46520d80628619b
operator1 <operator1@users.noreply.github.com>
2015-04-04 12:25:58 +0100

    Create AiffReader.java

---
+++ op1/src/main/java/com/op1/aiff/AiffReader.java
@@ -0,0 +1,53 @@
+package com.op1.aiff;
+
+import com.op1.iff.IffReader;
+import com.op1.iff.types.ID;
+
+import java.io.IOException;
+
+public class AiffReader {
+
+    private final IffReader iffReader;
+
+    public AiffReader(IffReader iffReader) {
+        this.iffReader = iffReader;
+    }
+
+    public Aiff readAiff() throws IOException {
+
+        final Aiff.Builder builder = new Aiff.Builder()
+                .withChunkId(iffReader.readID())
+                .withChunkSize(iffReader.readSignedLong())
+                .withFormat(iffReader.readID());
+
+        readChunks(builder);
+        return builder.build();
+    }
+
+    private void readChunks(Aiff.Builder builder) throws IOException {
+
+        while (true) {
+            try {
+                final ID id = iffReader.readID();
+                System.out.println(String.format("Found chunk: %s", id.getName()));
+
+                if (ChunkType.isKnownChunk(id.getName())) {
+                    readKnownChunk(builder, id);
+
+                } else {
+                    builder.withChunk(id, UnknownChunk.readUnknownChunk(iffReader, id));
+                }
+
+            } catch (Exception e) {
+                // TODO: don't use exceptions to control expected flow.
+                System.out.println("Reached end of stream");
+                break;
+            }
+        }
+    }
+
+    private void readKnownChunk(Aiff.Builder builder, ID id) throws IOException {
+        final ChunkType chunkType = ChunkType.fromName(id.getName());
+        builder.withChunk(id, chunkType.readChunk(iffReader));
+    }
+}
