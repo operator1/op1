59a737e08bb36c050cc6a052cef4961d2b0ccf0b
operator1 <operator1@users.noreply.github.com>
2015-04-04 12:30:58 +0100

    Create UnknownChunk.java

---
+++ op1/src/main/java/com/op1/aiff/UnknownChunk.java
@@ -0,0 +1,80 @@
+package com.op1.aiff;
+
+import com.op1.iff.Chunk;
+import com.op1.iff.IffReader;
+import com.op1.iff.types.ID;
+import com.op1.iff.types.SignedLong;
+import com.op1.util.Check;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class UnknownChunk implements Chunk {
+
+    private ID chunkId;
+    private SignedLong chunkSize;
+    private byte[] chunkData;
+
+    private UnknownChunk() {
+    }
+
+    private UnknownChunk(UnknownChunk chunk) {
+        this.chunkId = chunk.getChunkID();
+        this.chunkSize = chunk.getChunkSize();
+        this.chunkData = Arrays.copyOf(chunk.getChunkData(), chunk.getChunkData().length);
+    }
+
+    @Override
+    public ID getChunkID() {
+        return chunkId;
+    }
+
+    public SignedLong getChunkSize() {
+        return chunkSize;
+    }
+
+    public byte[] getChunkData() {
+        return chunkData;
+    }
+
+    public static class Builder {
+
+        private final UnknownChunk instance;
+
+        public Builder() {
+            instance = new UnknownChunk();
+        }
+
+        public UnknownChunk build() {
+            Check.notNull(instance.chunkId, "Missing chunkId");
+            Check.notNull(instance.chunkSize, "Missing chunkSize");
+            Check.notNull(instance.chunkData, "Missing chunkData");
+            return new UnknownChunk(instance);
+        }
+
+        public Builder withChunkId(ID chunkId) {
+            instance.chunkId = chunkId;
+            return this;
+        }
+
+        public Builder withChunkSize(SignedLong chunkSize) {
+            instance.chunkSize = chunkSize;
+            return this;
+        }
+
+        public Builder withChunkData(byte[] chunkData) {
+            instance.chunkData = chunkData;
+            return this;
+        }
+    }
+
+    public static UnknownChunk readUnknownChunk(IffReader iffReader, ID chunkId) throws IOException {
+        final SignedLong chunkSize = iffReader.readSignedLong();
+        final byte[] chunkData = iffReader.readBytes(chunkSize.toInt());
+        return new UnknownChunk.Builder()
+                .withChunkId(chunkId)
+                .withChunkSize(chunkSize)
+                .withChunkData(chunkData)
+                .build();
+    }
+}
