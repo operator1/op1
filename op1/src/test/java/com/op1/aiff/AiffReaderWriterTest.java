e20a1cc93be51c7953af93c8eb00420d41c48492
yoof <christian.venning@gmail.com>
2015-09-20 15:27:39 +0100

    Added an extra check in the AiffReaderWriterTest to check that the read file and the written file contain the same bytes.

--- op1/src/test/java/com/op1/aiff/AiffReaderWriterTest.java
+++ op1/src/test/java/com/op1/aiff/AiffReaderWriterTest.java
@@ -2,13 +2,15 @@ package com.op1.aiff;
 
 import com.op1.iff.IffReader;
 import com.op1.iff.IffWriter;
+import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
+import java.net.URL;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class AiffReaderWriterTest {
 
@@ -19,22 +21,22 @@ public class AiffReaderWriterTest {
 
     @Test
     public void read_and_write_album_file_produces_same_file() throws Exception {
-        doReadWriteTest(ALBUM_FILE);
+        doReadWriteTest(getFileFromClasspathResource(ALBUM_FILE));
     }
 
-    private void doReadWriteTest(String file) throws IOException {
+    @Test
+    public void newTest() throws Exception {
+
+        final File file = getFileFromClasspathResource(ALBUM_FILE);
+        assertTrue(file.exists());
+    }
+
+    private void doReadWriteTest(File readFile) throws IOException {
 
         // given
-        final InputStream inputStream = AiffReaderWriterTest.class.getClassLoader().getResourceAsStream(file);
-        final DataInputStream dataInputStream = new DataInputStream(inputStream);
-        final IffReader iffReader = new IffReader(dataInputStream);
-        final AiffReader aiffReader = new AiffReader(iffReader);
         final File writeFile = temporaryFolder.newFile("writeFile.aif");
-        System.out.println(writeFile.getAbsolutePath());
-        final FileOutputStream fileOutputStream = new FileOutputStream(writeFile);
-        final DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
-        final IffWriter iffWriter = new IffWriter(dataOutputStream);
-        final AiffWriter aiffWriter = new AiffWriter(iffWriter);
+        final AiffReader aiffReader = newAiffReader(readFile);
+        final AiffWriter aiffWriter = newAiffWriter(writeFile);
 
         // when
         final Aiff aiff = aiffReader.readAiff();
@@ -42,5 +44,30 @@ public class AiffReaderWriterTest {
 
         // then
         assertTrue(writeFile.exists());
+        assertTrue(FileUtils.contentEquals(readFile, writeFile));
+    }
+
+    private AiffWriter newAiffWriter(File writeFile) throws FileNotFoundException {
+        final FileOutputStream fileOutputStream = new FileOutputStream(writeFile);
+        final DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+        final IffWriter iffWriter = new IffWriter(dataOutputStream);
+        return new AiffWriter(iffWriter);
+    }
+
+    private AiffReader newAiffReader(File file) throws FileNotFoundException {
+        final FileInputStream fileInputStream = new FileInputStream(file);
+        final DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+        final IffReader iffReader = new IffReader(dataInputStream);
+        return new AiffReader(iffReader);
+    }
+
+    private File getFileFromClasspathResource(String classpathResource) {
+
+        final URL resource = AiffReaderWriterTest.class.getClassLoader().getResource(classpathResource);
+        if (resource == null) {
+            throw new IllegalArgumentException("Cannot find file: " + classpathResource);
+        }
+
+        return new File(resource.getFile());
     }
 }
